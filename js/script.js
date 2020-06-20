6b7df8fb7f3f0bfc790bb246dba4ad2917a42a5e
Taiana <taianakutueva17@gmail.com>
2020-06-20 18:29:23 +0300

    Исправляю путь к img и начинаю JS

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var search = document.querySelector(".legend-button");
+var searchSlide = document.querySelector(".modal-search");
+var dateArrival = document.querySelector(".date-arrival-item");
+var dateDeparture = document.querySelector(".date-departure-item");
+var searchForm = document.querySelector(".index-form");
+
+search.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  searchSlide.classList.add("modal-show");
+  dateArrival.focus();
+});
+
+searchForm.addEventListener("submit", function (evt) {
+    if (!dateArrival.value || !dateDeparture.value) {
+    evt.preventDefault();
+    } 
+});
+
