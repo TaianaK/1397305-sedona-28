1de668e603f1ad6735f9f7951bde8574e5669fbb
Taiana <taianakutueva17@gmail.com>
2020-06-21 23:19:22 +0300

    Выравниваю галочки

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-var search = document.querySelector(".legend-button");
+var searchButton = document.querySelector(".legend-button");
 var searchSlide = document.querySelector(".modal-search");
 var dateArrival = document.querySelector(".date-arrival-item");
 var dateDeparture = document.querySelector(".date-departure-item");
 var searchForm = document.querySelector(".index-form");
 
-search.addEventListener("click", function (evt) {
+searchButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   searchSlide.classList.add("modal-show");
   dateArrival.focus();
