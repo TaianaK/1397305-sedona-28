4a0c0386675f147bd6cb694a9b1726d9f5507e7b
Taiana <taianakutueva17@gmail.com>
2020-07-02 14:40:35 +0300

    Делаю анимацию и добавляю оптимизированные файлы

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 var searchButton = document.querySelector(".legend-button");
-var searchSlide = document.querySelector(".modal-search");
+var modalSearch = document.querySelector(".modal-search");
 var dateArrival = document.querySelector(".date-arrival-item");
 var dateDeparture = document.querySelector(".date-departure-item");
 var searchForm = document.querySelector(".index-form");
 
 searchButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  searchSlide.classList.add("modal-show");
-  dateArrival.focus();
+  modalSearch.classList.add("modal-close");
+  modalSearch.classList.remove("modal-error");
 });
 
 searchForm.addEventListener("submit", function (evt) {
     if (!dateArrival.value || !dateDeparture.value) {
     evt.preventDefault();
+    modalSearch.classList.add("modal-error");
     } 
 });
